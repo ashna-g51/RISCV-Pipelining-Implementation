eef655108ec1138cec4b2d05b18c0899f3e5f8c3
ashna-g51 <122644010+ashna-g51@users.noreply.github.com>
2024-07-23 03:00:12 +0530

    Update Data_Memory.v

--- src/Data_Memory.v
+++ src/Data_Memory.v
@@ -1,17 +1,3 @@
-// Copyright 2023 MERL-DSU
-
-//    Licensed under the Apache License, Version 2.0 (the "License");
-//    you may not use this file except in compliance with the License.
-//    You may obtain a copy of the License at
-
-//        http://www.apache.org/licenses/LICENSE-2.0
-
-//    Unless required by applicable law or agreed to in writing, software
-//    distributed under the License is distributed on an "AS IS" BASIS,
-//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//    See the License for the specific language governing permissions and
-//    limitations under the License.
-
 module Data_Memory(clk,rst,WE,WD,A,RD);
 
     input clk,rst,WE;
@@ -30,7 +16,6 @@ module Data_Memory(clk,rst,WE,WD,A,RD);
 
     initial begin
         mem[0] = 32'h00000000;
-        //mem[40] = 32'h00000002;
     end
 
 
